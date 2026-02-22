ecf415e1cea920b241d7e3c8a4d4cb2a03766678
usanema <nowik.tools@gmail.com>
2026-02-22 20:56:31 +0100

    feat: complete UI and database integration for intentions and participants

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  output: "server",
+  adapter: vercel()
 });
